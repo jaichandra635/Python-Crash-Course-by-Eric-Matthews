4d90d27b7d634548cbe25b0bf9fb89b54888ce73
jaichandra635 <97591346+jaichandra635@users.noreply.github.com>
2023-06-14 20:26:13 +0530

    Update 3.8.py

--- introducin_list/introducing_lists/3.8.py
+++ introducin_list/introducing_lists/3.8.py
@@ -1,1 +1,35 @@
 '''seeing the world:'''
+'''3.8 Seeing the World:'''
+
+# Step 1: Store the locations in a list in non-alphabetical order
+locations = ['bali', 'amazon', 'goa', 'maldives', 'dubai']
+
+# Step 2: Print the list in its original order
+print(locations)
+
+# Step 3: Print the list in alphabetical order using sorted()
+print(sorted(locations))
+
+# Step 4: Print the list to show it is still in its original order
+print(locations)
+
+# Step 5: Print the list in reverse alphabetical order using sorted()
+print(sorted(locations, reverse=True))
+
+# Step 6: Print the list to show it is still in its original order
+print(locations)
+
+# Step 7: Change the order of the list using reverse()
+locations.reverse()
+# Step 8: Print the list to show the changed order
+print(locations)
+
+# Step 9: Change the order of the list again using reverse()
+locations.reverse()
+# Step 9: Print the list to show it is back to its original order
+print(locations)
+
+# Step 10: Change the list to be stored in alphabetical order using sort()
+locations.sort()
+# Step 10: Print the list to show the changed order
+print(locations)
